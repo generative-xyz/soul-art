02283d04e4c269fad91c64f3d29de002303b2289
Dr. Cat <dr.cat911@generative.xyz>
2023-06-12 16:48:17 +0700

    update: arts

--- src/containers/Landing/Hero/index.tsx
+++ src/containers/Landing/Hero/index.tsx
@@ -1,18 +1,18 @@
 import s from './style.module.scss';
 import {Loading} from "@/containers/Landing/Loading";
-import Text from "@Animations/Text";
+// import Text from "@Animations/Text";
 
 export const Hero = (): JSX.Element => {
 
     return <div className={s.hero}>
         <div className={s.hero_bg}>
-            <img src={`https://storage.googleapis.com/generative-static-prod/soul-art/loading.jpeg`} alt="loading"/>
-        </div>
-        <div className="hero_inner">
-            <Text>
-                Welcome to New Bitcoin City
-            </Text>
+            <img src="https://storage.googleapis.com/generative-static-prod/soul-art/hero-bg-min.jpeg" alt="loading"/>
         </div>
         <Loading/>
+        {/*<div className={s.hero_inner}>*/}
+        {/*    <Text size={'d2'}>*/}
+        {/*        Welcome to New Bitcoin City*/}
+        {/*    </Text>*/}
+        {/*</div>*/}
     </div>
 }
--- src/containers/Landing/Loading/index.tsx
+++ src/containers/Landing/Loading/index.tsx
@@ -1,11 +1,31 @@
 import s from './style.module.scss';
+import classNames from "classnames";
+import Text from "@Animations/Text";
 
-export const Loading = ():JSX.Element =>{
+export const Loading = (): JSX.Element => {
 
     return <div className={s.loading}>
-            <div className={s.loading_icon}>
-                <img className={s.loading_icon_dark} src="https://storage.googleapis.com/generative-static-prod/soul-art/ic-loading-dark.svg" alt="ic-loading"/>
-                <img src="https://storage.googleapis.com/generative-static-prod/soul-art/ic-loading.svg" alt="ic-loading"/>
-            </div>
+        <div className={classNames(s.loading_over, s.loading_over__left)}>
+            <img src="https://storage.googleapis.com/generative-static-prod/soul-art/loading-left-min.png"
+                 alt="loading-left-min"/>
+        </div>
+        <div className={classNames(s.loading_over, s.loading_over__right)}>
+            <img src="https://storage.googleapis.com/generative-static-prod/soul-art/loading-right-min.png"
+                 alt="loading-right-min"/>
+        </div>
+        <div className={s.loading_icon}>
+            <img className={s.loading_icon_dark}
+                 src="https://storage.googleapis.com/generative-static-prod/soul-art/ic-loading-dark.svg"
+                 alt="ic-loading"/>
+            <img src="https://storage.googleapis.com/generative-static-prod/soul-art/ic-loading.svg" alt="ic-loading"/>
+        </div>
+        <div className={s.loading_inner}>
+            <Text size={'d2'} className={'mb-1_16'}>
+                Welcome to New Bitcoin City
+            </Text>
+            <Text size={'20'} className={'text-'}>
+                Let the tale of Souls inspire you.
+            </Text>
+        </div>
     </div>
 }
