f01287369610255519e5d60313ab57a4ae56baca
tlhngen <1103@generative.xyz>
2023-06-13 13:21:18 +0700

    fix: hero section

--- src/containers/Landing/Hero/index.tsx
+++ src/containers/Landing/Hero/index.tsx
@@ -1,9 +1,12 @@
+import Introduce from '../Introduce';
 import s from './style.module.scss';
-import {Loading} from "@/containers/Landing/Loading";
+import { Loading } from '@/containers/Landing/Loading';
 
 export const Hero = (): JSX.Element => {
-
-    return <div className={s.hero}>
-        <Loading/>
+  return (
+    <div className={s.hero}>
+      <Loading />
+      <Introduce />
     </div>
-}
+  );
+};
--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import {GridDebug} from '@Animations/Grid/grid';
-import {Hero} from './Hero';
-import Introduce from './Introduce';
+import { GridDebug } from '@Animations/Grid/grid';
+import { Hero } from './Hero';
 import Owner from './SectionOwner';
 import Tech from './Tech';
 import Flare from './Flare';
 import Living from './Lingving';
 import SubLiving from './SubLiving';
 import Sunback from './Sunback';
-import {useSmoothScroll} from "@Hooks/useSmoothScroll";
+import { useSmoothScroll } from '@Hooks/useSmoothScroll';
 
 export const LandingContainer: React.FC = () => {
-    useSmoothScroll();
-    return (
-        <>
-            <GridDebug/>
-            <Hero/>
-            <Introduce/>
-            <Living/>
-            <SubLiving/>
-            <Owner/>
-            <Flare/>
-            <Sunback/>
-            <Tech/>
-        </>
-    );
+  useSmoothScroll();
+  return (
+    <>
+      <GridDebug />
+      <Hero />
+      <Living />
+      <SubLiving />
+      <Owner />
+      <Flare />
+      <Sunback />
+      <Tech />
+    </>
+  );
 };
