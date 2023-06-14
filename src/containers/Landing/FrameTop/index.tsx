9b6f15c204cfe2f08adbb386fbeb5334e863a438
tlhngen <1103@generative.xyz>
2023-06-14 13:44:18 +0700

    fix: bug

--- src/containers/Landing/FrameTop/index.tsx
+++ src/containers/Landing/FrameTop/index.tsx
@@ -1,57 +1,72 @@
-import React, { useContext, useMemo, useRef } from "react";
-import { AnimateContext } from "@Context/Animate";
-import { Frames } from "@Animations/Frames";
-import s from "./styles.module.scss";
-import useWindowResize from "@Hooks/useWindowResize";
+import React, { useContext, useMemo, useRef } from 'react';
+import { AnimateContext } from '@Context/Animate';
+import { Frames } from '@Animations/Frames';
+import s from './styles.module.scss';
+import useWindowResize from '@Hooks/useWindowResize';
+import Living from '../Living';
+import SubLiving from '../SubLiving';
+import SectionOwner from '../SectionOwner';
+import Flare from '../Flare';
 
-export const FrameTop: React.FC = React.memo(() => {
-    const { registerLoader, unRegisterLoader } = useContext(AnimateContext);
+export const FrameTop: React.FC = () => {
+  const { registerLoader, unRegisterLoader } = useContext(AnimateContext);
 
-    // const lHeading = useRef<HTMLDivElement>(null);
-    const elMain = useRef<HTMLDivElement>(null);
-    //
-    // const lPart1 = useRef<HTMLDivElement>(null);
-    // const lPart2 = useRef<HTMLDivElement>(null);
-    // const lPart3 = useRef<HTMLDivElement>(null);
-    // const lPart4 = useRef<HTMLDivElement>(null);
-    //
-    // const headingFrame = useFrameProcessing(lHeading, 10, 20);
-    // const part1Frame = useFrameProcessing(lPart1, 55, 65, 100, 110);
-    // const part2Frame = useFrameProcessing(lPart2, 130, 140, 170, 180);
-    // const part3Frame = useFrameProcessing(lPart3, 200, 210, 245, 255);
-    // const part4Frame = useFrameProcessing(lPart4, 270, 280);
+  // const lHeading = useRef<HTMLDivElement>(null);
+  const elMain = useRef<HTMLDivElement>(null);
+  //
+  //   const lPart1 = useRef<HTMLDivElement>(null);
+  //   const lPart2 = useRef<HTMLDivElement>(null);
+  //   const lPart3 = useRef<HTMLDivElement>(null);
+  //   const lPart4 = useRef<HTMLDivElement>(null);
+  //
+  // const headingFrame = useFrameProcessing(lHeading, 10, 20);
+  // const part1Frame = useFrameProcessing(lPart1, 55, 65, 100, 110);
+  // const part2Frame = useFrameProcessing(lPart2, 130, 140, 170, 180);
+  // const part3Frame = useFrameProcessing(lPart3, 200, 210, 245, 255);
+  // const part4Frame = useFrameProcessing(lPart4, 270, 280);
 
-    const processing = frame => {
-        console.log('___ok');
-        // headingFrame(frame);
-        // part1Frame(frame);
-        // part2Frame(frame);
-        // part3Frame(frame);
-        // part4Frame(frame);
-    };
+  const processing = frame => {
+    console.log('___ok');
+    // headingFrame(frame);
+    // part1Frame(frame);
+    // part2Frame(frame);
+    // part3Frame(frame);
+    // part4Frame(frame);
+  };
 
-    const { isDesktop } = useWindowResize();
-    const urlFrame = useMemo((): string => {
-        return !isDesktop
-            ? `https://storage.googleapis.com/generative-static-prod/soul-art/sould-frames/%d.jpg`
-            : `https://storage.googleapis.com/generative-static-prod/soul-art/sould-frames/%d.jpg`;
-    }, [isDesktop]);
+  const { isDesktop } = useWindowResize();
+  const urlFrame = useMemo((): string => {
+    return !isDesktop
+      ? `https://storage.googleapis.com/generative-static-prod/soul-art/sould-frames/%d.jpg`
+      : `https://storage.googleapis.com/generative-static-prod/soul-art/sould-frames/%d.jpg`;
+  }, [isDesktop]);
 
-    return (
-        <div ref={elMain}>
-            <Frames
-                width={!isDesktop ? 1080 : 1920}
-                height={!isDesktop ? 1920 : 1080}
-                className={s.info_main}
-                urlFrame={urlFrame}
-                // webmFrame={`https://cdn.generative.xyz/pages/home/block-3-2/block-3-v2-%d.png.webp`}
-                totalFrames={129}
-                onProcessing={processing}
-                start={registerLoader}
-                end={unRegisterLoader}
-            >
-
-            </Frames>
+  return (
+    <div ref={elMain}>
+      <Frames
+        width={!isDesktop ? 1080 : 1920}
+        height={!isDesktop ? 1920 : 1080}
+        className={s.info_main}
+        urlFrame={urlFrame}
+        // webmFrame={`https://cdn.generative.xyz/pages/home/block-3-2/block-3-v2-%d.png.webp`}
+        totalFrames={129}
+        onProcessing={processing}
+        start={registerLoader}
+        end={unRegisterLoader}
+      >
+        <div className={s.livingArt}>
+          <Living />
         </div>
-    );
-});
+        <div className={s.subLiving}>
+          <SubLiving />
+        </div>
+        <div className={s.owner}>
+          <SectionOwner />
+        </div>
+        <div className={s.flare}>
+          <Flare />
+        </div>
+      </Frames>
+    </div>
+  );
+};
--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -4,7 +4,7 @@ import {Hero} from './Hero';
 import Owner from './SectionOwner';
 import Tech from './Tech';
 import Flare from './Flare';
-import Living from './Lingving';
+import Living from './Living';
 import SubLiving from './SubLiving';
 import Sunback from './Sunback';
 import {useSmoothScroll} from '@Hooks/useSmoothScroll';
