6a7e12223d380b6a316e088bd08d53b41d0edb25
kevin <0501@newbitcoincity.com>
2023-06-13 20:31:33 -0700

    feat: update card

--- src/components/SoulsCard/index.tsx
+++ src/components/SoulsCard/index.tsx
@@ -3,8 +3,8 @@ import { IMAGE_TYPE } from '../NFTDisplayBox/constant';
 import soulsCardStyles from './soulscard.module.scss';
 
 import Card from 'react-bootstrap/Card';
-import Button from '../Button';
 import Link from 'next/link';
+import cs from 'classnames';
 
 export interface INFTCard {
   href: string;
@@ -21,7 +21,7 @@ export interface INFTCard {
 const NFTCard = ({ href, image, className }: INFTCard) => {
   return (
     <Card className={className}>
-      <Card.Link href={href} as={Link}>
+      <Card.Link href={href} as={Link} className={soulsCardStyles.card_link}>
         <div className={soulsCardStyles.card_image__container}>
           <Card.Img
             variant="top"
@@ -30,12 +30,19 @@ const NFTCard = ({ href, image, className }: INFTCard) => {
           />
         </div>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk
-            of the cards content.
+          <Card.Text className={soulsCardStyles.card_label}>
+            Highest bid
           </Card.Text>
-          <Button>Go somewhere</Button>
+          <div
+            className={cs(
+              'd-flex justify-content-between align-items-center',
+              soulsCardStyles.card_body__content
+            )}
+          >
+            <Card.Text>1.5 BTC</Card.Text>
+
+            <Card.Text>2d : 16h : 12m</Card.Text>
+          </div>
         </Card.Body>
       </Card.Link>
     </Card>
