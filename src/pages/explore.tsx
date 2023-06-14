a72ce68d896633f42a524f5b65b92af927047563
kevin <0501@newbitcoincity.com>
2023-06-14 03:16:39 -0700

    fix: rename souls page to explore page

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,32 +1,11 @@
-import { ROUTE_PATH } from '@/constants/route-path';
-import Artifacts from '@/containers/Artifacts';
+import { SoulsContainer } from '@/containers/Souls';
 import Layout from '@/layouts';
-import { getAccessToken } from '@/utils/auth-storage';
-import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
-import sessionStorage from '@/utils/sessionstorage';
+import { FC } from 'react';
 
-const HomePage: NextPage = () => {
-  const router = useRouter();
-  const isLogin = getAccessToken();
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleRedirect = useCallback(() => {
-    const firstTimeVisit = sessionStorage.get('firstTimeVisit');
-    if ((firstTimeVisit || firstTimeVisit === null) && !isLogin) {
-      router.push(ROUTE_PATH.ABOUT);
-      sessionStorage.set('firstTimeVisit', false);
-    } else {
-      setIsLoading(false);
-    }
-  }, [router, isLogin, setIsLoading]);
-
-  useEffect(() => {
-    handleRedirect();
-  }, [handleRedirect]);
-
-  return <Layout>{!isLoading && <Artifacts />}</Layout>;
+const SoulsPage: FC = () => {
+  return <Layout>
+    <SoulsContainer />
+  </Layout>;
 };
 
-export default HomePage;
+export default SoulsPage;
