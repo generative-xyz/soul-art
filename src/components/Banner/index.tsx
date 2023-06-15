8d3921130ac98ca814c4539d59aadbfbf0ac74b3
kevin <0501@newbitcoincity.com>
2023-06-15 16:14:45 -0700

    refactor: update banner button

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,16 +2,21 @@ import { ARROW_URL } from '@/constants/asset';
 import Button from '../Button';
 import IconSVG from '../IconSVG';
 import s from './style.module.scss';
+import Link from 'next/link';
+import { ROUTE_PATH } from '@/constants/route-path';
 
 const Banner = () => {
   return (
     <div className={s.banner}>
       <p className={s.bannerText}>
-        Posuere dolor adipiscing est et potenti odio condimentum euismod lobortis.{' '}
+        Posuere dolor adipiscing est et potenti odio condimentum euismod
+        lobortis.{' '}
       </p>
       <Button borderRadius="100px" className={s.bannerButton}>
-        Claim Souls
-        <IconSVG src={ARROW_URL} maxWidth="12" maxHeight="8" />
+        <Link href={`${ROUTE_PATH.CLAIM}`} className={s.banner_link}>
+          Claim Souls
+          <IconSVG src={ARROW_URL} maxWidth="12" maxHeight="8" />
+        </Link>
       </Button>
     </div>
   );
