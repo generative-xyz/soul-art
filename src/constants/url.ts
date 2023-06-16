90ac24408e28cec21cbe51807dc01ff005120c8c
tlhngen <1103@generative.xyz>
2023-06-16 14:58:02 +0700

    fix: video bg

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,10 @@
 export const XVERSE_DOWNLOAD_URL = 'https://www.xverse.app/download';
 export const TC_EXPLORER = 'https://explorer.trustless.computer';
+export const WEBM_VIDEO_URL1 =
+  'https://storage.googleapis.com/generative-static-prod/soul-art/output.webm';
+export const MP4_VIDEO_URL1 =
+  'https://storage.googleapis.com/generative-static-prod/soul-art/output.mp4';
+export const WEBM_VIDEO_URL2 =
+  'https://storage.googleapis.com/generative-static-prod/soul-art/output.webm';
+export const MP4_VIDEO_URL2 =
+  'https://storage.googleapis.com/generative-static-prod/soul-art/output.mp4';
--- src/containers/Landing/Flare/index.tsx
+++ src/containers/Landing/Flare/index.tsx
@@ -1,53 +1,39 @@
 import TextAnimate from '@/components/TextAnimate';
 import s from './style.module.scss';
-import { Frames } from '@Animations/Frames';
-import { useRef, useContext } from 'react';
-import { AnimateContext } from '@/contexts/Animate';
-import { useFrameProcessing } from '@/hooks/useFrameProcessing';
 import { Container } from 'react-bootstrap';
-import { CDN_URL } from '@/configs';
+import VideoBg from '../VideoBg';
+import { WEBM_VIDEO_URL1, MP4_VIDEO_URL1 } from '@Constants/url';
+import Text from '@/animations/Text';
+import { AnimFade } from '@/animations/Fade';
 
 const Flare: React.FC = () => {
-  const { registerLoader, unRegisterLoader } = useContext(AnimateContext);
-
-  const lPart1 = useRef<HTMLDivElement>(null);
-
-  const part1Frame = useFrameProcessing(lPart1, 10, 20, 119, 129);
-
-  const processing = (frame: number) => {
-    part1Frame(frame);
-  };
   return (
     <div className={s.main}>
-      <Frames
-        width={1920}
-        height={1080}
-        className={s.info_main}
-        urlFrame={`${CDN_URL}/sould-frames/%d.jpg`}
-        // webmFrame={`https://cdn.generative.xyz/pages/home/block-3-2/block-3-v2-%d.png.webp`}
-        totalFrames={129}
-        onProcessing={processing}
-        start={registerLoader}
-        end={unRegisterLoader}
-      >
-        <div ref={lPart1} className={s.flareSection}>
+      <VideoBg videoUrls={{ mp4: MP4_VIDEO_URL1, webm: WEBM_VIDEO_URL1 }}>
+        <div className={s.flareSection}>
           <Container className={s.container}>
-            <div className={s.wrapContent}>
-              <p className={s.sectionContent}>
+            <div className={s.wrapSectionContent}>
+              <Text
+                animOption={{ screen: 0, offset: 0.5, type: 'paragraph' }}
+                className={s.sectionContent}
+              >
                 But beware, for as with any tale, there were challenges along
                 the way. When doubt and uncertainty clouded the hearts of the
                 believers, the sun&apos;s radiance waned, casting shadows upon
                 the artworks.
-              </p>
-              <a href="" className={s.tag}>
-                <TextAnimate>
-                  <span>Lorem ipsum dolor</span>
-                </TextAnimate>
-              </a>
+              </Text>
+
+              <AnimFade offset={0.7}>
+                <a href="" className={s.tag}>
+                  <TextAnimate>
+                    <span>Lorem ipsum dolor</span>
+                  </TextAnimate>
+                </a>
+              </AnimFade>
             </div>
           </Container>
         </div>
-      </Frames>
+      </VideoBg>
     </div>
   );
 };
