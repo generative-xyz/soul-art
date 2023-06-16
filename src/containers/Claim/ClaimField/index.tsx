818ecd7214400c86c5154e61272b96f48054baff
sain <2004@generative.xyz>
2023-06-16 12:28:57 +0700

    fix: error

--- src/containers/Claim/ClaimField/index.tsx
+++ src/containers/Claim/ClaimField/index.tsx
@@ -1,20 +1,22 @@
-// import IconSVG from '@/components/IconSVG';
 import IconSVG from '@/components/IconSVG';
 import s from './style.module.scss';
 import { notiReceive } from '@/constants/asset';
-// import { notiReceive } from '@/constants/asset';
 type IClainFeildProps = {
   handleClaimed: () => void;
+  handleConnectWallet: () => void;
   isConnectedWallet: boolean;
   isReceiveAble: boolean;
+  isWaitingForConfirm: boolean;
 };
 
 const ClaimField: React.FC<IClainFeildProps> = ({
   handleClaimed,
+  handleConnectWallet,
   isConnectedWallet,
   isReceiveAble,
+  isWaitingForConfirm,
 }) => {
-  // Notification Smart Contract
+  //Notification Smart Contract
   const NotificationConnectWallet: React.FC = () => {
     return (
       <div className={s.noti}>
@@ -26,14 +28,21 @@ const ClaimField: React.FC<IClainFeildProps> = ({
 
   const ContentConnected: React.FC = () => {
     return (
-      <div className={`${s.textButton} ${isReceiveAble ? '' : s.false}`}>
-        <span>Claim Soul</span>
+      <div
+        className={`${s.textButton} ${isReceiveAble ? '' : s.false}`}
+        onClick={handleClaimed}
+      >
+        {isWaitingForConfirm ? (
+          <span>Wait for your confirmation...</span>
+        ) : (
+          <span>Claim Soul</span>
+        )}
       </div>
     );
   };
   const ContentNotConnected: React.FC = () => {
     return (
-      <div className={s.textButton} onClick={handleClaimed}>
+      <div className={s.textButton} onClick={handleConnectWallet}>
         <span>Connect wallet to claim Souls</span>
       </div>
     );
@@ -42,7 +51,9 @@ const ClaimField: React.FC<IClainFeildProps> = ({
   return (
     <div className={s.claimField}>
       {isConnectedWallet ? <ContentConnected /> : <ContentNotConnected />}
-      {isReceiveAble ? '' : <NotificationConnectWallet />}
+      {!isReceiveAble && isConnectedWallet ? (
+        <NotificationConnectWallet />
+      ) : null}
     </div>
   );
 };
