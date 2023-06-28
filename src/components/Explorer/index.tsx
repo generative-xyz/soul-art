347d4a9f75913a70ac2b78abdc66182c0964a3b6
dr.cat <dr.cat911@generative.xyz>
2023-06-28 11:11:26 +0700

    fix: iframe review

--- src/components/Explorer/index.tsx
+++ src/components/Explorer/index.tsx
@@ -1,47 +1,55 @@
-import React, { ForwardedRef, useImperativeHandle, useRef } from 'react';
+import React, {ForwardedRef, useEffect, useImperativeHandle, useRef, useState} from 'react';
 import styles from './style.module.scss';
 
 interface IRef {
-  reloadIframe: () => void;
-  getHtmlIframe: () => HTMLIFrameElement | null;
+    reloadIframe: () => void;
+    getHtmlIframe: () => HTMLIFrameElement | null;
 }
 
 interface IProps {
-  url: string;
-  onLoaded?: () => void;
+    url: string;
+    onLoaded?: () => void;
 }
 
 const Explorer = React.forwardRef<IRef, IProps>(
-  (props: IProps, ref: ForwardedRef<IRef>) => {
-    const { url, onLoaded } = props;
-    const iframeRef = useRef<HTMLIFrameElement>(null);
+    (props: IProps, ref: ForwardedRef<IRef>) => {
+        const {url, onLoaded} = props;
+        const iframeRef = useRef<HTMLIFrameElement>(null);
+        const [isReady, setIsReady] = useState<boolean>(false);
 
-    const reloadIframe = () => {
-      if (iframeRef.current) {
-        // eslint-disable-next-line no-self-assign
-        iframeRef.current.src = iframeRef.current.src;
-      }
-    };
+        const reloadIframe = () => {
+            if (iframeRef.current) {
+                // eslint-disable-next-line no-self-assign
+                iframeRef.current.src = iframeRef.current.src;
+            }
+        };
 
-    const getHtmlIframe = (): HTMLIFrameElement | null => {
-      return iframeRef.current;
-    };
+        const getHtmlIframe = (): HTMLIFrameElement | null => {
+            return iframeRef.current;
+        };
 
-    useImperativeHandle(ref, () => ({
-      reloadIframe,
-      getHtmlIframe,
-    }));
+        useImperativeHandle(ref, () => ({
+            reloadIframe,
+            getHtmlIframe,
+        }));
 
-    return (
-      <iframe
-        ref={iframeRef}
-        className={styles.explorer}
-        src={url}
-        style={{ overflow: 'hidden' }}
-        onLoad={onLoaded}
-      ></iframe>
-    );
-  }
+        useEffect(() => {
+            const rect = iframeRef.current?.getBoundingClientRect();
+            if (rect?.width !== 0 && rect?.height !== 0) {
+                setIsReady(true);
+            }
+        }, [iframeRef])
+
+        return (
+            <iframe
+                ref={iframeRef}
+                className={styles.explorer}
+                src={isReady ? url : ''}
+                style={{overflow: 'hidden'}}
+                onLoad={onLoaded}
+            ></iframe>
+        );
+    }
 );
 
 export default Explorer;
