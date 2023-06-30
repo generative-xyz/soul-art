f6e5af00c671b24c06dcaa612f1751c7f7dfbb39
benjiro2077 <benjiro2077@generative.xyz>
2023-06-30 14:54:46 +0700

    feat: new 404 page

--- src/containers/404/404.styled.ts
+++ src/containers/404/404.styled.ts
@@ -1,25 +1,24 @@
 import { px2rem } from '@trustless-computer/dapp-core';
 import styled from 'styled-components';
 
-const Container = styled.div`
+export const Container = styled.div`
   height: calc(100vh - 80px);
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: ${px2rem(48)};
+  gap: ${px2rem(32)};
 
   .image {
-    max-height: ${px2rem(400)};
+    max-height: ${px2rem(192)};
     object-fit: contain;
+    margin-left: ${px2rem(20)};
   }
 
   .page-title {
     font-weight: 500;
-    font-size: ${px2rem(40)};
-    line-height: 120%;
-    color: #fff;
+    font-size: ${px2rem(28)};
+    line-height: 140%;
+    color: #5B5B5B;
     text-align: center;
   }
 `;
-
-export { Container };
