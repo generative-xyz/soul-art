a495d4204ac5be4a8226a8ae1829610ef0abe574
luin <1103@generative.xyz>
2023-07-06 10:36:18 +0700

    fix: update content tech landingpage

--- src/containers/Tech/index.tsx
+++ src/containers/Tech/index.tsx
@@ -4,47 +4,11 @@ import { HEADER_HEIGHT } from '@/layouts';
 import { Col, Container } from 'react-bootstrap';
 import { CDN_URL } from '@/configs';
 import Text from '@/animations/Text';
-import Item from './Item';
 import Headline from './Headline';
 import Link from 'next/link';
 import IconSVG from '@/components/IconSVG';
 
 const TechLandingPage = () => {
-  const itemArray = [
-    {
-      title: 'Souls Lifecycle',
-      leftImg: `${CDN_URL}/tech/techImg1.png`,
-      leftContent: 'Living Souls',
-      middleIcon: `${CDN_URL}/tech/grid-circle.svg`,
-      middleContent: 'Abandoned',
-      rightImg: `${CDN_URL}/tech/techImg2.png`,
-      rightContent: 'Living Souls',
-      arrowRevert: false,
-      isCustom: false,
-      type: 1,
-    },
-    {
-      title: 'Souls Orphanage',
-      leftImg: `${CDN_URL}/tech/techImg3.png`,
-      leftContent: 'Adoption List',
-      middleIcon: `${CDN_URL}/tech/GM.svg`,
-      middleContent: 'Adoption interest',
-      rightImg: `${CDN_URL}/tech/techImg4.png`,
-      rightContent: 'Living Souls',
-      arrowRevert: true,
-      type: 2,
-    },
-    {
-      title: 'Souls DAO',
-      leftImg: `${CDN_URL}/tech/techImg5.svg`,
-      middleIcon: `${CDN_URL}/tech/hand-holding-dollar.svg`,
-      middleContent: 'Funds',
-      rightImg: `${CDN_URL}/tech/techImg6.svg`,
-      arrowRevert: false,
-      type: 'p',
-    },
-  ];
-
   return (
     <div className={s.techLandingPage}>
       <Header height={HEADER_HEIGHT} isAnimation={false} theme={''} />
@@ -57,48 +21,36 @@ const TechLandingPage = () => {
               title={'The Souls Cryptoart Protocol'}
             />
 
-            <div className={s.wrapContent_content}>
-              <Text as={'p'} size={'24'} className={s.content_desc}>
-                The Souls protocol is designed to provide an ever-lasting
-                infrastructure to support the Souls artworks. At the core of the
-                protocol is a set of Solidity smart contracts running on Bitcoin
-                via the Trustless Computer protocol.
-              </Text>
-              <Text as={'p'} size={'24'} className={s.content_desc}>
-                These smart contracts act as the central coordinator,
-                orchestrating the interactions between Souls and their
-                collectors. These smart contracts ensure the smooth functioning
-                of the Souls collection, handling interactions such as Souls
-                adoption, Souls visualization, and DAO governance.
-              </Text>
-            </div>
+            <div className={`${s.wrapContent_content} ${s.wrapContent_flex}`}>
+              <div className={s.leftContent}>
+                <Text as={'p'} size={'24'} className={s.content_desc}>
+                  The Souls protocol is designed to provide an ever-lasting
+                  infrastructure to support the Souls artworks. At the core of
+                  the protocol is a set of Solidity smart contracts running on
+                  Bitcoin via the Trustless Computer protocol.
+                </Text>
+                <Text as={'p'} size={'24'} className={s.content_desc}>
+                  These smart contracts act as the central coordinator,
+                  orchestrating the interactions between Souls and their
+                  collectors. These smart contracts ensure the smooth
+                  functioning of the Souls collection, handling interactions
+                  such as Souls adoption, Souls visualization, and DAO
+                  governance.
+                </Text>
+                <Text as={'p'} size={'24'} className={s.content_desc}>
+                  The Souls DAO serves as a decentralized governance mechanism,
+                  allowing collectors to make decisions and steer the direction
+                  of the Souls protocol. Through the DAO, collectors can propose
+                  and vote on important matters, including changes to the Soul
+                  orphanage rules, community initiatives, and resource
+                  allocation.
+                </Text>
+              </div>
 
-            {itemArray.map(
-              ({
-                title,
-                leftImg,
-                leftContent,
-                middleIcon,
-                middleContent,
-                rightImg,
-                rightContent,
-                arrowRevert,
-                type,
-              }) => (
-                <Item
-                  key={title}
-                  title={title}
-                  leftImg={leftImg}
-                  leftContent={leftContent}
-                  middleIcon={middleIcon}
-                  middleContent={middleContent}
-                  rightImg={rightImg}
-                  rightContent={rightContent}
-                  arrowRevert={arrowRevert}
-                  type={type}
-                />
-              )
-            )}
+              <div className={s.rightContent}>
+                <img src={`${CDN_URL}/tech/tech1.png`} alt="image" />
+              </div>
+            </div>
 
             <div className={s.wrapContent_bottom}>
               <img src={`${CDN_URL}/tech/Dark%20(1).jpg`} alt="image" />
@@ -128,7 +80,7 @@ const TechLandingPage = () => {
             </Text>
 
             <div className={s.imgMedium}>
-              <img src={`${CDN_URL}/tech/techBlock2.jpg`} alt="image" />
+              <img src={`${CDN_URL}/tech/tech3.png`} alt="image" />
             </div>
             <Text size={'24'} className={`${s.text} ${s.mb_32}`}>
               The Souls collection embraces the concept of being fully on-chain,
@@ -138,7 +90,7 @@ const TechLandingPage = () => {
             </Text>
 
             <div className={s.imgFull}>
-              <img src={`${CDN_URL}/tech/techCode1.jpg`} alt="image" />
+              <img src={`${CDN_URL}/tech/code_01.jpg`} alt="image" />
             </div>
             <Text size={'24'} className={`${s.text} ${s.mb_32}`}>
               p5.js—a powerful JavaScript library for creative coding—enables
@@ -151,7 +103,7 @@ const TechLandingPage = () => {
             </Text>
 
             <div className={s.imgFull}>
-              <img src={`${CDN_URL}/tech/techCode2.jpg`} alt="image" />
+              <img src={`${CDN_URL}/tech/code_02.jpg`} alt="image" />
             </div>
 
             <Text size={'24'} className={`${s.text}`}>
